5a85b1a8289d1883e7eb78c610cd12b2043e1b96
MikeBar0n <barondaniel2001@gmail.com>
2024-08-08 00:18:30 -0500

    Se crea el proyecto en NeatBeans

---
+++ DivisionNumeros/src/main/java/com/mycompany/divisionnumeros/DivisionNumeros.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.divisionnumeros;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class DivisionNumeros {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
